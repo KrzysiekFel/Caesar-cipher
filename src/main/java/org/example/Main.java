edbd81b91bdb66d80404b3c73eae689d14d9a270
Krzysiek <krzysztof.feliksiak@gmail.com>
2024-09-01 18:24:45 +0200

    gui skeleton and encrypt, decrypt and brute

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        Cipher cipher = new Cipher();
+        System.out.println(cipher.encrypt("Kris it's my name.", 32));
+        System.out.println(cipher.decrypt("Ovmwcmxdwcq!creqi ", 100));
+        System.out.println(cipher.decryptByBruteForce("Ovmw"));
     }
 }
