6e2360165483608d823493c9174f71636e77fd54
Krzysiek <krzysztof.feliksiak@gmail.com>
2024-09-01 20:10:44 +0200

    file management added

--- src/main/java/org/example/FileManager.java
+++ src/main/java/org/example/FileManager.java
@@ -1,15 +1,32 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
 public class FileManager {
 
     public String readFile(String filePath) {
-        return filePath;
+        StringBuilder contentBuilder = new StringBuilder();
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get(filePath));
+            for (String line : allLines) {
+                contentBuilder.append(line).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file at " + filePath, e);
+        }
+        return contentBuilder.toString();
     }
 
     public void writeFile(String content, String filePath) {
-    }
-
-    public boolean isFileExists(String filePath) {
-        return false;
+        try {
+            Files.writeString(Paths.get(filePath), content, StandardOpenOption.CREATE,
+                                                            StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException("Error writing file at " + filePath, e);
+        }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,14 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Cipher cipher = new Cipher();
-        System.out.println(cipher.encrypt("Hi, Kris here! Do you like java?.", 100));
+        System.out.println(cipher.encrypt("Hi, Kris here! Do you like java?", 100));
         System.out.println(cipher.decrypt("Lm'cOvmwcliviacHsc!sycpmoicnezeb", 100));
         System.out.println(cipher.decryptByBruteForce("Lm'cOvmwcliviacHsc!sycpmoicnezeb"));
         System.out.println("=======");
         System.out.println(cipher.decryptByStatistics("Lm'cOvmwcliviacHsc!sycpmoicnezeb"));
+        System.out.println("=======");
+        FileManager fileManager = new FileManager();
+        System.out.println(fileManager.readFile("src/main/resources/input.txt"));
+        fileManager.writeFile("test content!", "src/main/resources/test_output.txt");
     }
 }
