a637002a961a1b3dbe8c057718708dff4784882a
Krzysiek <krzysztof.feliksiak@gmail.com>
2024-09-01 12:48:44 +0200

    Cipher file

--- src/main/java/org/example/Cipher.java
+++ src/main/java/org/example/Cipher.java
@@ -1,2 +1,23 @@
-package org.example;public class Cipher {
+package org.example;
+
+public class Cipher {
+    private char[] alphabet;
+
+    public Cipher(char[] alphabet) {
+        this.alphabet = alphabet;
+    }
+
+    public Cipher() {
+        this.alphabet = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+                'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    }
+
+    public String encrypt(String text, int shift) {
+        return text;
+    }
+
+    public String decrypt(String encryptedText, int shift) {
+        return encryptedText;
+    }
+
 }
